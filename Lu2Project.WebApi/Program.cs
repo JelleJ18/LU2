33cd9eb14ec9ff6ddc1b38920e5b18b4a7ac08a8
Jelle Jankowski <jag.jankowski@student.avans.nl>
2025-03-12 00:10:13 +0100

    identity framework configuration

--- Lu2Project.WebApi/Program.cs
+++ Lu2Project.WebApi/Program.cs
@@ -1,35 +1,56 @@
 using Lu2Project.WebApi.Repositories;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
+builder.Services.AddOpenApi(); // OpenAPI (Swagger)
 builder.Services.AddScoped<IEnvironmentRepository, EnvironmentRepository>();
 
-var sqlConnectionString= builder.Configuration.GetValue<string>("SqlConnectionString");
+// Controleer of de SQL Connection String is gevonden
+var sqlConnectionString = builder.Configuration.GetValue<string>("SqlConnectionString");
 var sqlConnectionStringFound = !string.IsNullOrWhiteSpace(sqlConnectionString);
 Console.WriteLine($"SqlConnectionString: {sqlConnectionString}");
 Console.WriteLine($"SqlConnectionString found: {sqlConnectionStringFound}");
 
+// Configureer Identity
+builder.Services.AddAuthorization();
+builder.Services
+    .AddIdentityApiEndpoints<IdentityUser>(options =>
+    {
+        options.User.RequireUniqueEmail = true; // Vereist een uniek e-mailadres
+        options.Password.RequiredLength = 10;   // Minimale wachtwoordlengte
+        options.Password.RequireDigit = true;   // Vereist een cijfer
+        options.Password.RequireLowercase = true; // Vereist een kleine letter
+        options.Password.RequireUppercase = true; // Vereist een hoofdletter
+        options.Password.RequireNonAlphanumeric = true; // Vereist een speciaal teken
+    })
+    .AddRoles<IdentityRole>() // Voeg ondersteuning voor rollen toe
+    .AddDapperStores(options =>
+    {
+        options.ConnectionString = builder.Configuration.GetConnectionString("DapperIdentity"); // Gebruik de connection string
+    });
+
 var app = builder.Build();
 
+// Configureer de HTTP-request pipeline
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+// Map Identity API endpoints
+app.MapGroup("/account").MapIdentityApi<IdentityUser>();
+
+// Homepage-route
 app.MapGet("/", () => $"The API is up. Connection string found: {(sqlConnectionStringFound ? "yes" : "no")}");
 
-// Configure the HTTP request pipeline.
+// OpenAPI (Swagger) alleen in development
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+// Vereis autorisatie voor alle controllers
+app.MapControllers().RequireAuthorization();
 
 app.Run();
